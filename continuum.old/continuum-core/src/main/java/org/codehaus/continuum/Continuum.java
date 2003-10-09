b63a100a72374f8e8b378733dff37363ff2f6792
pdonald <pdonald@7d1aff33-b372-0410-9c12-f5b3bce2e4c1>
2003-10-09 04:00:38 +0000

    Removed access qualifiers that dont need to be present in interfaces and whos presence goes against JLS recomendations

--- continuum.old/continuum-core/src/main/java/org/codehaus/continuum/Continuum.java
+++ continuum.old/continuum-core/src/main/java/org/codehaus/continuum/Continuum.java
@@ -2,19 +2,15 @@ package org.codehaus.plexus.continuum;
 
 import org.apache.maven.Model;
 
-
 /**
- *
- *
  * @author <a href="mailto:jason@plexus.org">Jason van Zyl</a>
- *
- * @version $Id: Continuum.java,v 1.1.1.1 2003-09-01 16:06:00 jvanzyl Exp $
+ * @version $Id: Continuum.java,v 1.2 2003-10-09 04:00:38 pdonald Exp $
  */
 public interface Continuum
 {
     /** role of component. */
-    static String ROLE = Continuum.class.getName();
+    String ROLE = Continuum.class.getName();
 
-    public void addModel( Model model );
+    void addModel( Model model );
 }
 
