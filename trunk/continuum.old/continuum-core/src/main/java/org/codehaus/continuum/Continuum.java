ae78ce049d8155ea2d75c7159dba858a54a1c2f7
pdonald <pdonald@7d1aff33-b372-0410-9c12-f5b3bce2e4c1>
2003-10-12 01:14:56 +0000

    Revert all the changes done today

--- trunk/continuum.old/continuum-core/src/main/java/org/codehaus/continuum/Continuum.java
+++ trunk/continuum.old/continuum-core/src/main/java/org/codehaus/continuum/Continuum.java
@@ -4,10 +4,13 @@ import org.apache.maven.Model;
 
 /**
  * @author <a href="mailto:jason@plexus.org">Jason van Zyl</a>
- * @version $Id: Continuum.java,v 1.3 2003-10-12 00:55:39 pdonald Exp $
+ * @version $Id: Continuum.java,v 1.4 2003-10-12 01:14:55 pdonald Exp $
  */
 public interface Continuum
 {
+    /** role of component. */
+    String ROLE = Continuum.class.getName();
+
     void addModel( Model model );
 }
 
