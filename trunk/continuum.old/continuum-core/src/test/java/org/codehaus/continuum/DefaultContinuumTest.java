0115cf48c52dcf78718ac51b99afab5236a03e18
jvanzyl <jvanzyl@7d1aff33-b372-0410-9c12-f5b3bce2e4c1>
2004-01-16 06:20:14 +0000

    o updating to use the new compiler component

--- trunk/continuum.old/continuum-core/src/test/java/org/codehaus/continuum/DefaultContinuumTest.java
+++ trunk/continuum.old/continuum-core/src/test/java/org/codehaus/continuum/DefaultContinuumTest.java
@@ -2,13 +2,6 @@ package org.codehaus.plexus.continuum;
 
 import org.codehaus.plexus.PlexusTestCase;
 
-/**
- *
- * 
- * @author <a href="mailto:jason@plexus.org">Jason van Zyl</a>
- *
- * @version $Id: DefaultContinuumTest.java,v 1.3 2003-10-12 01:14:56 pdonald Exp $
- */
 public class DefaultContinuumTest
     extends PlexusTestCase
 {
