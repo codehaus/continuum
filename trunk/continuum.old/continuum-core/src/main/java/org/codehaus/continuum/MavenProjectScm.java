67f9306a16e47ae03668a39c3eb893d26804c0a2
jvanzyl <jvanzyl@7d1aff33-b372-0410-9c12-f5b3bce2e4c1>
2004-01-17 23:16:51 +0000

    o simplifying after making some changes to maven-scm

--- trunk/continuum.old/continuum-core/src/main/java/org/codehaus/continuum/MavenProjectScm.java
+++ trunk/continuum.old/continuum-core/src/main/java/org/codehaus/continuum/MavenProjectScm.java
@@ -1,13 +1,7 @@
 package org.codehaus.plexus.continuum;
 
-import org.apache.maven.genericscm.command.Command;
-import org.apache.maven.genericscm.command.checkout.CheckOutCommand;
-import org.apache.maven.genericscm.command.tag.TagCommand;
-import org.apache.maven.genericscm.command.update.UpdateCommand;
 import org.apache.maven.genericscm.manager.DefaultScmManager;
 import org.apache.maven.genericscm.manager.ScmManager;
-import org.apache.maven.genericscm.provider.cvslib.CvsScmFactory;
-import org.apache.maven.genericscm.repository.RepositoryInfo;
 import org.apache.maven.project.Project;
 
 /**
@@ -15,7 +9,7 @@ import org.apache.maven.project.Project;
  *
  * @author <a href="mailto:jason@maven.org">Jason van Zyl</a>
  *
- * @version $Id: MavenProjectScm.java,v 1.2 2004-01-16 21:25:16 jvanzyl Exp $
+ * @version $Id: MavenProjectScm.java,v 1.3 2004-01-17 23:16:51 jvanzyl Exp $
  */
 public class MavenProjectScm
 {
@@ -24,15 +18,7 @@ public class MavenProjectScm
     public MavenProjectScm( Project project )
         throws Exception
     {
-        RepositoryInfo repositoryInfo = new RepositoryInfo();
-
-        repositoryInfo.setUrl( project.getRepository().getConnection() );
-
-        scmManager = new DefaultScmManager( repositoryInfo );
-
-        // I should not have to do this, it should be done by the factory
-        // when the repo information is set.
-        scmManager.addScmFactory( new CvsScmFactory() );
+        scmManager = new DefaultScmManager( project.getRepository().getConnection() );
     }
 
     public void checkout( String directory )
@@ -40,13 +26,7 @@ public class MavenProjectScm
     {
         try
         {
-            System.out.println( "checking out sources!" );
-
-            Command command = scmManager.getCommand( CheckOutCommand.NAME );
-
-            command.setWorkingDirectory( directory );
-
-            command.execute();
+            scmManager.checkout( directory );
         }
         catch ( Exception e )
         {
@@ -61,32 +41,11 @@ public class MavenProjectScm
     {
         try
         {
-            Command command = scmManager.getCommand( UpdateCommand.NAME );
-
-            command.setWorkingDirectory( directory );
-
-            command.execute();
+            scmManager.update( directory );
         }
         catch ( Exception e )
         {
             throw new ContinuumException( "Cannot checkout sources: ", e );
         }
     }
-
-    // This is not supported by SCM yet.
-    public void tag( String tagId )
-        throws ContinuumException
-    {
-        try
-        {
-            Command command = scmManager.getCommand( TagCommand.NAME );
-
-            command.execute();
-        }
-        catch ( Exception e )
-        {
-            throw new ContinuumException( "Cannot checkout sources: ", e );
-        }
-    }
-
 }
