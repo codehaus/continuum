ce8a6914fa16e518641f8271a89e5c9d1b74c100
jvanzyl <jvanzyl@7d1aff33-b372-0410-9c12-f5b3bce2e4c1>
2004-01-18 22:56:00 +0000

    o adding a couple little scripts that i'm using for testing, just using nc   to toss messages to the simple network registrar and trigger.

---
+++ continuum.old/continuum-core/register.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+#echo /home/jvanzyl/js/org.apache.maven/maven-components/maven-model/project.xml | nc localhost 10001
+#echo /home/jvanzyl/js/org.apache.maven/maven-components/maven-model-xpp3/project.xml | nc localhost 10001
+#echo /home/jvanzyl/js/org.apache.maven/maven-components/maven-project/project.xml | nc localhost 10001
+echo /home/jvanzyl/js/org.apache.maven/maven-components/maven-core/project.xml | nc localhost 10001
