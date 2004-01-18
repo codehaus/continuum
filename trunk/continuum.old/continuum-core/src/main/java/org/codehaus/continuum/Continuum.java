b5d96b259ce16f72582a46b102e764ec33b65add
jvanzyl <jvanzyl@7d1aff33-b372-0410-9c12-f5b3bce2e4c1>
2004-01-18 07:27:27 +0000

    o creating triggers and separating the alarm clock from the main continuum component.

--- trunk/continuum.old/continuum-core/src/main/java/org/codehaus/continuum/Continuum.java
+++ trunk/continuum.old/continuum-core/src/main/java/org/codehaus/continuum/Continuum.java
@@ -7,5 +7,13 @@ public interface Continuum
     String ROLE = Continuum.class.getName();
 
     void addProject( Project project );
+
+    void addProject( String url );
+
+    void buildProject( String groupId, String artifactId )
+        throws Exception;
+
+    void buildProjects()
+        throws Exception;
 }
 
