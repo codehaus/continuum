4709e8af69897a5cc53f596cd13415fab7cab9e9
jvanzyl <jvanzyl@7d1aff33-b372-0410-9c12-f5b3bce2e4c1>
2004-01-18 18:23:03 +0000

    *** empty log message ***

--- continuum.old/continuum-core/src/main/java/org/codehaus/continuum/registration/AbstractContinuumRegistrar.java
+++ continuum.old/continuum-core/src/main/java/org/codehaus/continuum/registration/AbstractContinuumRegistrar.java
@@ -1,15 +1,17 @@
 package org.codehaus.plexus.continuum.registration;
 
 import org.codehaus.plexus.continuum.Continuum;
+import org.codehaus.plexus.logging.AbstractLogEnabled;
 
 /**
  *
- * 
+ *
  * @author <a href="mailto:jason@maven.org">Jason van Zyl</a>
  *
- * @version $Id: AbstractContinuumRegistrar.java,v 1.1 2004-01-18 18:01:40 jvanzyl Exp $
+ * @version $Id: AbstractContinuumRegistrar.java,v 1.2 2004-01-18 18:23:03 jvanzyl Exp $
  */
 public class AbstractContinuumRegistrar
+    extends AbstractLogEnabled
 {
     private Continuum continuum;
 
