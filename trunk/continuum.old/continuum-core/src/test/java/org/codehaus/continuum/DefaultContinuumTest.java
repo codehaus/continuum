4451f2a44065b4196bdde5e2f7e33f9f36709d12
jvanzyl <jvanzyl@7d1aff33-b372-0410-9c12-f5b3bce2e4c1>
2004-02-02 20:47:05 +0000

    o taking the test configuration and making it the base plexus.xml file that   will be used for the first release.
    o change the DefaultContinuumTest so that it looks for the standard
      configuration for testing.

--- trunk/continuum.old/continuum-core/src/test/java/org/codehaus/continuum/DefaultContinuumTest.java
+++ trunk/continuum.old/continuum-core/src/test/java/org/codehaus/continuum/DefaultContinuumTest.java
@@ -2,6 +2,8 @@ package org.codehaus.plexus.continuum;
 
 import org.codehaus.plexus.PlexusTestCase;
 
+import java.io.InputStream;
+
 public class DefaultContinuumTest
     extends PlexusTestCase
 {
@@ -14,6 +16,15 @@ public class DefaultContinuumTest
         throws Exception
     {
         super.setUp();
+    }
+
+    protected InputStream getCustomConfiguration()
+    {
+        ClassLoader cl = Thread.currentThread().getContextClassLoader();
+
+        InputStream is = cl.getResourceAsStream( "org/codehaus/plexus/continuum/plexus.xml" );
+
+        return is;
     }
 
     public void testContinuum()
