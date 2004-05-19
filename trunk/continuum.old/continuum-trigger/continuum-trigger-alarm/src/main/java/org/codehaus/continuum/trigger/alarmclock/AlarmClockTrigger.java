c9326e4659fd527852f21352fbb43c6da874608e
trygvis <trygvis@7d1aff33-b372-0410-9c12-f5b3bce2e4c1>
2004-05-19 22:19:13 +0000

    o Improving.

--- trunk/continuum.old/continuum-trigger/continuum-trigger-alarm/src/main/java/org/codehaus/continuum/trigger/alarmclock/AlarmClockTrigger.java
+++ trunk/continuum.old/continuum-trigger/continuum-trigger-alarm/src/main/java/org/codehaus/continuum/trigger/alarmclock/AlarmClockTrigger.java
@@ -1,18 +1,17 @@
 package org.codehaus.continuum.trigger.alarmclock;
 
-import org.codehaus.plexus.personality.plexus.lifecycle.phase.Initializable;
-import org.codehaus.plexus.personality.plexus.lifecycle.phase.Startable;
-import org.codehaus.continuum.trigger.AbstractContinuumTrigger;
-
 import java.util.Timer;
 import java.util.TimerTask;
 
+import org.codehaus.continuum.ContinuumException;
+import org.codehaus.continuum.trigger.AbstractContinuumTrigger;
+import org.codehaus.plexus.personality.plexus.lifecycle.phase.Initializable;
+import org.codehaus.plexus.personality.plexus.lifecycle.phase.Startable;
+
 /**
- *
- *
  * @author <a href="mailto:jason@maven.org">Jason van Zyl</a>
  *
- * @version $Id: AlarmClockTrigger.java,v 1.2 2004-05-13 17:48:17 trygvis Exp $
+ * @version $Id: AlarmClockTrigger.java,v 1.3 2004-05-19 22:19:13 trygvis Exp $
  */
 public class AlarmClockTrigger
     extends AbstractContinuumTrigger
@@ -20,32 +19,48 @@ public class AlarmClockTrigger
 {
     private int interval;
 
+    private int delay;
+
     private Timer timer;
 
-    /** @see org.codehaus.plexus.personality.plexus.lifecycle.phase.Initializable#initialize */
+    ///////////////////////////////////////////////////////////////////////////
+    // Plexus Component Implementation
+
     public void initialize()
         throws Exception
     {
+        if ( interval <= 0 )
+            throw new ContinuumException( "Invalid value for 'interval': the interval must be bigger that 0." );
+
+        if ( delay <= 0 )
+            throw new ContinuumException( "Invalid value for 'delay': the delay must be bigger that 0." );
+
+        if ( getContinuum() == null )
+            throw new ContinuumException( "Missing requirement: 'continuum'." );
+
         timer = new Timer();
     }
 
-    /** @see org.codehaus.plexus.personality.plexus.lifecycle.phase.Startable#start */
     public void start()
         throws Exception
     {
-        getLogger().info( "Starting AlarmClockTrigger with an interval of " + interval + " seconds" );
+        if ( interval == 1 )
+            getLogger().info( "Starting AlarmClockTrigger with an interval of " + interval + " second" );
+        else
+            getLogger().info( "Starting AlarmClockTrigger with an interval of " + interval + " seconds" );
 
-        timer.schedule( new BuildTask(), 0, interval * 60 * 1000 );
+        timer.schedule( new BuildTask(), delay * 1000, interval * 1000 );
     }
 
-    /** @see org.codehaus.plexus.personality.plexus.lifecycle.phase.Startable#stop */
     public void stop()
         throws Exception
     {
-
     }
 
-    class BuildTask
+    ///////////////////////////////////////////////////////////////////////////
+    // Alarm Clock Trigger implementation
+
+    private class BuildTask
         extends TimerTask
     {
         public void run()
