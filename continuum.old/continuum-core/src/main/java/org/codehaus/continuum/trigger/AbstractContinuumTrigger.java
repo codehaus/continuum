398f81c0535fa7aedf147826f7a2fe305d26e2fa
trygvis <trygvis@7d1aff33-b372-0410-9c12-f5b3bce2e4c1>
2004-07-14 05:33:34 +0000

    o Stylefix.

--- continuum.old/continuum-core/src/main/java/org/codehaus/continuum/trigger/AbstractContinuumTrigger.java
+++ continuum.old/continuum-core/src/main/java/org/codehaus/continuum/trigger/AbstractContinuumTrigger.java
@@ -4,11 +4,9 @@ import org.codehaus.continuum.Continuum;
 import org.codehaus.plexus.logging.AbstractLogEnabled;
 
 /**
- *
- *
  * @author <a href="mailto:jason@maven.org">Jason van Zyl</a>
  *
- * @version $Id: AbstractContinuumTrigger.java,v 1.2 2004-05-13 17:48:17 trygvis Exp $
+ * @version $Id: AbstractContinuumTrigger.java,v 1.3 2004-07-14 05:33:34 trygvis Exp $
  */
 public class AbstractContinuumTrigger
     extends AbstractLogEnabled
