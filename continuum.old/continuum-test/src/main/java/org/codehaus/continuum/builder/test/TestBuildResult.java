16dc435e2c2f2bd11ea46bfc500ad675fe91170a
trygvis <trygvis@7d1aff33-b372-0410-9c12-f5b3bce2e4c1>
2004-07-29 04:27:42 +0000

    o Moving TestProjectDescriptor to org.codehaus.continuum.builder.test. o The command will prepend the basedir of the scm repo base. o Added default constructor to TestBuildResult.

--- continuum.old/continuum-test/src/main/java/org/codehaus/continuum/builder/test/TestBuildResult.java
+++ continuum.old/continuum-test/src/main/java/org/codehaus/continuum/builder/test/TestBuildResult.java
@@ -1,27 +1,57 @@
 package org.codehaus.continuum.builder.test;
 
 /*
- * LICENSE
+ * Copyright (c) 2004, Jason van Zyl and Trygve Laugstøl
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 import org.codehaus.continuum.project.AbstractContinuumBuildResult;
+import org.codehaus.continuum.project.ContinuumBuild;
 
 /**
  * @author <a href="mailto:trygvis@inamo.no">Trygve Laugst&oslash;l</a>
- * @version $Id: TestBuildResult.java,v 1.1 2004-07-27 00:06:10 trygvis Exp $
+ * @version $Id: TestBuildResult.java,v 1.2 2004-07-29 04:27:41 trygvis Exp $
  */
 public class TestBuildResult
     extends AbstractContinuumBuildResult
 {
     private boolean success;
 
-    public TestBuildResult( boolean success )
+    public TestBuildResult()
     {
+    }
+
+    public TestBuildResult( ContinuumBuild build, boolean success )
+    {
+        super( build );
+
         this.success = success;
     }
 
     public boolean isSuccess()
     {
         return success;
+    }
+
+    private void setSuccess( boolean success )
+    {
+        this.success = success;
     }
 }
--- continuum.old/continuum-test/src/test/java/org/codehaus/continuum/test/scm/TestScmTest.java
+++ continuum.old/continuum-test/src/test/java/org/codehaus/continuum/test/scm/TestScmTest.java
@@ -1,7 +1,25 @@
 package org.codehaus.continuum.test.scm;
 
 /*
- * LICENSE
+ * Copyright (c) 2004, Jason van Zyl and Trygve Laugstøl
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 import java.io.File;
@@ -15,7 +33,7 @@ import org.codehaus.plexus.util.FileUtils;
 
 /**
  * @author <a href="mailto:trygvis@inamo.no">Trygve Laugst&oslash;l</a>
- * @version $Id: TestScmTest.java,v 1.1 2004-07-27 00:06:12 trygvis Exp $
+ * @version $Id: TestScmTest.java,v 1.2 2004-07-29 04:27:42 trygvis Exp $
  */
 public class TestScmTest
     extends PlexusTestCase
@@ -41,7 +59,7 @@ public class TestScmTest
     {
         ScmManager scmManager = (ScmManager) lookup( ScmManager.ROLE );
 
-        String scmUrl = "scm:test:" + getTestFile( "src/test/repositories") + ":test-repo";
+        String scmUrl = "scm:test:src/test/repositories:test-repo";
 
         scmManager.setRepositoryInfo( scmUrl );
 
