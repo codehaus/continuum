a1fd09f6dac1324d2111612413fd20704890c582
trygvis <trygvis@7d1aff33-b372-0410-9c12-f5b3bce2e4c1>
2004-10-07 11:45:26 +0000

    *** empty log message ***

--- continuum.old/continuum-web/update.sh
+++ continuum.old/continuum-web/update.sh
@@ -21,23 +21,24 @@ rm -rf $runtime/application
 
 mkdir -p $runtime/apps/maven2
 cp -r $M2_HOME/* $runtime/apps/maven2
-cp ~/.maven/repository/maven/jars/maven-core-2.0-SNAPSHOT.jar $runtime/apps/maven2/lib
-cp ~/.maven/repository/maven/jars/maven-artifact-2.0-SNAPSHOT.jar $runtime/apps/maven2/core
-cp ~/.maven/repository/plexus/jars/plexus-log4j-logging-1.0-SNAPSHOT.jar $runtime/core
+cp ~/.maven/repository/classworlds/jars/classworlds-1.1-SNAPSHOT.jar $runtime/apps/maven2/core/
+cp ~/.maven/repository/plexus/jars/plexus-0.17-SNAPSHOT.jar $runtime/apps/maven2/core
+cp ~/.maven/repository/xpp3/jars/xpp3-1.1.3.3.jar $runtime/apps/maven2/core
+cp ~/.maven/repository/xstream/jars/xstream-1.0-SNAPSHOT.jar $runtime/apps/maven2/core
+
+#cp ~/.maven/repository/maven/jars/maven-artifact-2.0-SNAPSHOT.jar $runtime/apps/maven2/core
+#cp ~/.maven/repository/plexus/jars/plexus-log4j-logging-1.0-SNAPSHOT.jar $runtime/core
+
 cp ~/.maven/repository/maven/jars/maven-artifact-2.0-SNAPSHOT.jar $runtime/apps/maven2/lib
-rm -f $runtime/apps/maven2/core/plexus-0.17*.jar
-cp ~/.maven/repository/plexus/jars/plexus-0.17-SNAPSHOT.jar $runtime/apps/maven2/lib
-cp ~/.maven/repository/plexus/jars/plexus-artifact-container-1.0-alpha-1-SNAPSHOT.jar $runtime/apps/maven2/lib
+cp ~/.maven/repository/maven/jars/maven-core-2.0-SNAPSHOT.jar $runtime/apps/maven2/lib
+cp ~/.maven/repository/maven/jars/maven-model-2.0-SNAPSHOT.jar $runtime/apps/maven2/lib
+cp ~/.maven/repository/maven/jars/maven-plugin-2.0-SNAPSHOT.jar $runtime/apps/maven2/lib
 cp ~/.maven/repository/maven/jars/wagon-api-1.0-alpha-1-SNAPSHOT.jar $runtime/apps/maven2/lib
 cp ~/.maven/repository/maven/jars/wagon-http-lightweight-1.0-alpha-1-SNAPSHOT.jar $runtime/apps/maven2/lib
-
-cp ~/.maven/repository/maven/plugins/maven-pom-plugin-1.0-SNAPSHOT.jar $runtime/apps/maven2/repository/maven/plugins/maven-pom-plugin-1.0-SNAPSHOT.jar
-
-cp ~/.maven/repository/plexus/jars/plexus-0.17-SNAPSHOT.jar $runtime/core
-cp ~/.maven/repository/plexus/jars/plexus-artifact-container-1.0-alpha-1-SNAPSHOT.jar $runtime/apps/maven2/lib/
+cp ~/.maven/repository/plexus/jars/plexus-artifact-container-1.0-alpha-1-SNAPSHOT.jar $runtime/apps/maven2/lib
+cp ~/.maven/repository/plexus/jars/plexus-i18n-1.0-beta-3.jar $runtime/apps/maven2/lib
 
 cp ~/.maven/repository/classworlds/jars/classworlds-1.1-SNAPSHOT.jar $runtime/core/boot/
-cp ~/.maven/repository/classworlds/jars/classworlds-1.1-SNAPSHOT.jar $runtime/apps/maven2/core/
 
 cp ~/.maven/repository/plexus/jars/plexus-compiler-api-1.0.jar $runtime/apps/continuumweb/lib
 cp ~/.maven/repository/plexus/jars/plexus-compiler-javac-1.0.jar $runtime/apps/continuumweb/lib
@@ -49,9 +50,6 @@ cp ~/.maven/repository/plexus/jars/plexus-jetty-1.0-beta-1.jar $runtime/apps/con
 cp ~/.maven/repository/plexus/jars/plexus-servlet-1.0-beta-2-SNAPSHOT.jar $runtime/apps/continuumweb/lib
 cp ~/.maven/repository/plexus/jars/plexus-velocity-1.0-beta-3.jar $runtime/apps/continuumweb/lib
 
-cp ~/.maven/repository/plexus/jars/plexus-artifact-container-1.0-alpha-1-SNAPSHOT.jar $runtime/apps/maven2/lib
-cp ~/.maven/repository/plexus/jars/plexus-i18n-1.0-beta-3.jar $runtime/apps/maven2/lib
-
 # Web tools
 cp ~/.maven/repository/plexus/jars/plexus-formica-1.0-beta-2.jar $runtime/apps/continuumweb/lib
 cp ~/.maven/repository/plexus/jars/plexus-summit-1.0-beta-3.jar $runtime/apps/continuumweb/lib
@@ -62,7 +60,7 @@ cp ~/.maven/repository/commons-fileupload/jars/commons-fileupload-1.0-beta-1.jar
 #cp ~/.maven/repository/commons-fileupload/jars/commons-fileupload-1.0.jar $runtime/apps/continuumweb/lib
 
 # Prevayler deps
-cp ~/.maven/repository/prevayler/jars/prevayler-2.02.005.jar $runtime/apps/maven2/lib
+#cp ~/.maven/repository/prevayler/jars/prevayler-2.02.005.jar $runtime/apps/maven2/lib
 
 chmod +x $runtime/apps/maven2/bin/*
 
@@ -100,8 +98,8 @@ cp ../continuum-api/target/*.jar $lib
 cp ../continuum-core/target/*.jar $lib
 cp ../continuum-builder/continuum-builder-maven2/target/*.jar $lib
 cp ../continuum-trigger/continuum-trigger-alarm/target/*.jar $lib
+cp ../continuum-trigger/continuum-trigger-socket/target/*.jar $lib
 cp ../continuum-store/continuum-store-hibernate/target/*.jar $lib
-cp ../continuum-store/continuum-store-prevayler/target/*.jar $lib
 cp ../continuum-notifier/continuum-notifier-mail/target/*.jar $lib
 cp ../continuum-web/target/*.jar $lib
 cp ../continuum-standalone/target/*.jar $lib
