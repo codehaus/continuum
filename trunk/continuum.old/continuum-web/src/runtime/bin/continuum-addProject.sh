66a108045ae2c5ed0a67651716bf23c49928c625
trygvis <trygvis@7d1aff33-b372-0410-9c12-f5b3bce2e4c1>
2004-10-20 17:06:08 +0000

    *** empty log message ***

--- trunk/continuum.old/continuum-web/src/runtime/bin/continuum-addProject.sh
+++ trunk/continuum.old/continuum-web/src/runtime/bin/continuum-addProject.sh
@@ -1,35 +1,11 @@
 #!/bin/bash
 
-projectName=$1
-scmConnection=$2
-nagEmailAddress=$3
-version=$4
+url=$1
 
-if [ -z "$projectName" ]
+if [ -z "$url" ]
 then
-  read -p "Project name:" projectName
+  read -p "url:" url
 fi
 
-if [ -z "$scmConnection" ]
-then
-  read -p "SCM url:" scmConnection
-fi
-
-if [ -z "$nagEmailAddress" ]
-then
-  read -p "Nag email address :" nagEmailAddress
-fi
-
-if [ -z "$version" ]
-then
-  read -p "Version:" version
-fi
-
-type="maven2"
-
-echo "$projectName
-$scmConnection
-$nagEmailAddress
-$version
-$type
-" | nc localhost 6789
+echo "$url
+" | nc localhost 6790
