a9e3dea0d6e15d077010d6657508a78820c70a87
trygvis <trygvis@7d1aff33-b372-0410-9c12-f5b3bce2e4c1>
2004-10-20 17:04:05 +0000

    o Using the new MavenUtil.

--- continuum.old/continuum-web/src/main/java/org/codehaus/continuum/web/action/AddProjectPomUrl.java
+++ continuum.old/continuum-web/src/main/java/org/codehaus/continuum/web/action/AddProjectPomUrl.java
@@ -22,78 +22,34 @@ package org.codehaus.continuum.web.action;
  * SOFTWARE.
  */
 
-import java.io.File;
 import java.net.URL;
 import java.util.Iterator;
 import java.util.Map;
 
-import org.apache.maven.model.CiManagement;
-import org.apache.maven.model.Scm;
-import org.apache.maven.project.MavenProject;
-
-import org.codehaus.continuum.ContinuumException;
-import org.codehaus.continuum.builder.ContinuumBuilder;
-import org.codehaus.continuum.builder.maven2.Maven2ContinuumBuilder;
+import org.codehaus.continuum.maven2.Maven2Utils;
 import org.codehaus.continuum.web.utils.WebUtils;
-import org.codehaus.plexus.personality.plexus.lifecycle.phase.Initializable;
 import org.codehaus.plexus.summit.SummitConstants;
 import org.codehaus.plexus.summit.rundata.RunData;
 import org.codehaus.plexus.summit.view.ViewContext;
-import org.codehaus.plexus.util.FileUtils;
-import org.codehaus.plexus.util.IOUtil;
-import org.codehaus.plexus.util.StringUtils;
 
 /**
  * @author <a href="mailto:trygvis@inamo.no">Trygve Laugst&oslash;l</a>
- * @version $Id: AddProjectPomUrl.java,v 1.4 2004-10-15 13:01:10 trygvis Exp $
+ * @version $Id: AddProjectPomUrl.java,v 1.5 2004-10-20 17:04:05 trygvis Exp $
  */
 public class AddProjectPomUrl
     extends AbstractAction
-    implements Initializable
 {
     /** @requirement */
-    private ContinuumBuilder m2Builder;
-
-    public void initialize()
-        throws Exception
-    {
-        if ( !( m2Builder instanceof Maven2ContinuumBuilder ) )
-        {
-            throw new Exception( "The builder has to be a Maven2ContinuumBuilder." );
-        }
-    }
+    private Maven2Utils maven2Utils;
 
     public void execute( Map request )
         throws Exception
     {
-        String urlString = (String) request.get( "addProject.pomUrl" );
+        String url = (String) request.get( "addProject.pomUrl" );
 
-        getLogger().info( "Downloading '" + urlString + "'." );
+        getLogger().info( "Adding project from '" + url + "'." );
 
-        File pomFile;
-
-        // download the pom.
-        URL url = new URL( urlString );
-
-        String pom = IOUtil.toString( url.openStream() );
-
-        pomFile = File.createTempFile( "continuum-", "-pom-download" );
-
-        FileUtils.fileWrite( pomFile.getAbsolutePath(), pom );
-
-        MavenProject project = ((Maven2ContinuumBuilder) m2Builder).getProject( pomFile );
-
-        String name = getName( project );
-
-        String scmUrl = getScmUrl( project );
-
-        String nagEmailAddress = getNagEmailAddress( project );
-
-        String version = getVersion( project );
-
-        getLogger().info( "Adding '" + name + "', scm url: '" + scmUrl + "', nag email address: '" + nagEmailAddress + "'." );
-
-        getContinuum().addProject( name, scmUrl, nagEmailAddress, version, "maven2" );
+        maven2Utils.addProjectFromUrl( new URL( url ) );
 
         RunData data = (RunData) request.get( "data" );
 
@@ -102,70 +58,5 @@ public class AddProjectPomUrl
         Iterator projects = getContinuumStore().getAllProjects();
 
         vc.put( "projects", WebUtils.projectsToProjectModels( getI18N(), projects ) );
-    }
-
-    private String getName( MavenProject project )
-    {
-        String name = project.getName();
-
-        if ( StringUtils.isEmpty( name ) )
-        {
-            name = project.getGroupId() + ":" + project.getArtifactId();
-        }
-
-        return name;
-    }
-
-    private String getScmUrl( MavenProject project )
-        throws ContinuumException
-    {
-        Scm scm = project.getScm();
-
-        if ( scm == null )
-        {
-            throw new ContinuumException( "Missing Scm from the project descriptor." );
-        }
-
-        String url = scm.getConnection();
-
-        if ( StringUtils.isEmpty( url ) )
-        {
-            throw new ContinuumException( "Missing anonymous scm connection url." );
-        }
-
-        return url;
-    }
-
-    private String getNagEmailAddress( MavenProject project )
-        throws ContinuumException
-    {
-        CiManagement ciManagement = project.getCiManagement();
-
-        if ( ciManagement == null )
-        {
-            throw new ContinuumException( "Missing CiManagement from the project descriptor." );
-        }
-
-        String nagEmailAddress = ciManagement.getNagEmailAddress();
-
-        if ( StringUtils.isEmpty( nagEmailAddress ) )
-        {
-            throw new ContinuumException( "Missing nag email address from the continuous integration info." );
-        }
-
-        return nagEmailAddress;
-    }
-
-    private String getVersion( MavenProject project )
-        throws ContinuumException
-    {
-        String version = project.getVersion();
-
-        if ( StringUtils.isEmpty( version ) )
-        {
-            throw new ContinuumException( "Missing version from the project descriptor." );
-        }
-
-        return version;
     }
 }
