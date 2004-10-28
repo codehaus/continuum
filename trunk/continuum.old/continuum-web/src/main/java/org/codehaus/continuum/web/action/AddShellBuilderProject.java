a2900396fd25afa0e3083caef9f5aad1b426bc8b
jvanzyl <jvanzyl@7d1aff33-b372-0410-9c12-f5b3bce2e4c1>
2004-10-28 18:36:13 +0000

    no message

--- trunk/continuum.old/continuum-web/src/main/java/org/codehaus/continuum/web/action/AddShellBuilderProject.java
+++ trunk/continuum.old/continuum-web/src/main/java/org/codehaus/continuum/web/action/AddShellBuilderProject.java
@@ -1,7 +1,7 @@
 package org.codehaus.continuum.web.action;
 
 /*
- * Copyright (c) 2004, Jason van Zyl and Trygve Laugstøl
+ * Copyright (c) 2004, Jason van Zyl and Trygve Laugstï¿½l
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
@@ -22,24 +22,37 @@ package org.codehaus.continuum.web.action;
  * SOFTWARE.
  */
 
-import java.net.URL;
-import java.util.Iterator;
-import java.util.Map;
-
-import org.codehaus.continuum.maven2.Maven2Utils;
+import org.codehaus.continuum.Continuum;
+import org.codehaus.continuum.ContinuumException;
+import org.codehaus.continuum.builder.ContinuumBuilder;
+import org.codehaus.continuum.store.tx.StoreTransactionManager;
 import org.codehaus.continuum.web.utils.WebUtils;
 import org.codehaus.plexus.summit.SummitConstants;
 import org.codehaus.plexus.summit.rundata.RunData;
 import org.codehaus.plexus.summit.view.ViewContext;
+import org.codehaus.plexus.util.FileUtils;
+import org.codehaus.plexus.util.IOUtil;
+import org.codehaus.plexus.util.xml.Xpp3Dom;
+import org.codehaus.plexus.util.xml.Xpp3DomBuilder;
+
+import java.io.File;
+import java.io.FileReader;
+import java.net.URL;
+import java.util.Iterator;
+import java.util.Map;
 
 /**
  * @author <a href="mailto:trygvis@inamo.no">Trygve Laugst&oslash;l</a>
- * @version $Id: AddShellBuilderProject.java,v 1.1 2004-10-28 16:47:48 jvanzyl Exp $
+ * @version $Id: AddShellBuilderProject.java,v 1.2 2004-10-28 18:36:13 jvanzyl Exp $
  */
 public class AddShellBuilderProject
     extends AbstractAction
 {
-    private Maven2Utils maven2Utils;
+    private Continuum continuum;
+
+    private ContinuumBuilder builder;
+
+    private StoreTransactionManager txManager;
 
     public void execute( Map request )
         throws Exception
@@ -48,7 +61,7 @@ public class AddShellBuilderProject
 
         getLogger().info( "Adding project from '" + url + "'." );
 
-        maven2Utils.addProjectFromUrl( new URL( url ) );
+        addProjectFromUrl( new URL( url ) );
 
         RunData data = (RunData) request.get( "data" );
 
@@ -57,5 +70,62 @@ public class AddShellBuilderProject
         Iterator projects = getContinuumStore().getAllProjects();
 
         vc.put( "projects", WebUtils.projectsToProjectModels( getI18N(), projects ) );
+    }
+
+    public String addProjectFromUrl( URL url )
+        throws ContinuumException
+    {
+        try
+        {
+            String pom = IOUtil.toString( url.openStream() );
+
+            File pomFile = File.createTempFile( "continuum-", "-pom-download" );
+
+            FileUtils.fileWrite( pomFile.getAbsolutePath(), pom );
+
+            Xpp3Dom project = Xpp3DomBuilder.build( new FileReader( pomFile ) );
+
+            String name = project.getChild( "name" ).getValue();
+
+            if ( name == null )
+            {
+                throw new ContinuumException( "Must have a name." );
+            }
+
+            String scmUrl = project.getChild( "repository" ).getChild( "connection" ).getValue();
+
+            if ( scmUrl == null )
+            {
+                throw new ContinuumException( "Must have a repository/connection element." );
+            }
+
+            String nagEmailAddress = project.getChild( "build" ).getChild( "nagEmailAddress" ).getValue();
+
+            if ( nagEmailAddress == null )
+            {
+                throw new ContinuumException( "Must have a build/nagEmailAddress." );
+            }
+
+            String version = project.getChild( "currentVersion" ).getValue();
+
+            if ( version == null )
+            {
+                throw new ContinuumException( "Must have a currentVersion element." );
+            }
+
+            txManager.enter();
+
+            String projectId = continuum.addProject( name, scmUrl, nagEmailAddress, version, "maven" );
+
+            txManager.leave();
+
+            return projectId;
+        }
+        catch ( Exception ex )
+        {
+            txManager.rollback();
+
+            throw new ContinuumException( "Error while adding propject.", ex );
+        }
     }
 }
